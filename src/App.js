8b08d89b25c8ac019cbbd4d6fba8f609dd135416
Connor <kor.connor@gmail.com>
2025-05-22 00:33:24 +0900

    CH03_16. input 상태 관리와 useRef - useRef

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 import CustomList from "./components/CustomList";
 import Button from "./components/Button";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import MainHeader from "./components/MainHeader";
 
 function App() {
-  const [text, setText] = useState("Hello");
+  const [name, setName] = useState({
+    first: "Connor",
+    last: "Kim"
+  })
+  const firstNameRef = useRef()
 
-  const onChange = (e) => {
-    const inputText = e.target.value;
-    setText(inputText);
-  };
-
+  const confirm = () => {
+    console.log(firstNameRef.current.value);
+  }
+  
   return (
     <div className="App">
-      <h1>{text}</h1>
-      <input onChange={onChange}></input>
+      <h1>{name.last} {name.first}</h1>
+      <input name="성" placeholder="성"></input>
+      <input name="이름" placeholder="이름" ref={firstNameRef}></input>
+      <button onClick={confirm}>확인</button>
     </div>
   );
 }
