ba6fcfd6ab7d0c2312398a709209d7d700299237
Connor <kor.connor@gmail.com>
2025-05-21 23:31:35 +0900

    feat: CH03_11. 컴포넌트의 속성(props)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MainHeader from "./components/MainHeader";
+import CustomList from "./components/CustomList";
 
 function App() {
+  const array1 = ["apple", "banana", "orange"];
   return (
     <div className="App">
-      <MainHeader></MainHeader>
+      <ul>
+        {array1.map((v) => {
+          return <CustomList text={v}></CustomList>;
+        })}
+      </ul>
     </div>
   );
 }
--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,5 +1,9 @@
-function MainHeader() {
-  return <h1>Hello world!</h1>;
+import React from "react";
+
+function MainHeader(props) {
+  // 상태를 처리하는 함수 
+  // [상태 데이터, 상태를 위한 setter 함수]
+  return <h1>{props.text}</h1>; // arrow function ES6 문법 
 }
 
 export default MainHeader;
