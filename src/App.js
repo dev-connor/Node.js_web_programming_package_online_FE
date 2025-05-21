f6f0e4e0be1effd50712d4a84e7c0fe2cb4571ef
Connor <kor.connor@gmail.com>
2025-05-22 08:54:00 +0900

    CH03_18. useEffect 와 마운트, 언바운트

--- src/App.js
+++ src/App.js
@@ -2,40 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import CustomList from "./components/CustomList";
 import Button from "./components/Button";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MainHeader from "./components/MainHeader";
 
 function App() {
-  const [tasks, setTask] = useState([
-    {
-      id: 0,
-      text: "강의 듣기",
-    },
-  ]);
-  const inputRef = useRef();
+  const [secound, setSecound] = useState(0);
 
-  const confirm = () => {
-    const text = inputRef.current.value;
-    setTask([
-      ...tasks,
-      {
-        id: tasks[tasks.length - 1].id + 1,
-        text,
-      },
-    ]);
-  };
+  // 마운트 이후 실행
+  useEffect(() => {
+    let interval = setInterval(() => {
+      console.log(secound);
+      setSecound(secound + 1);
+    }, 1000);
+
+    // 언마운트
+    return () => {
+      clearInterval(interval);
+    };
+  }, [secound]);
 
   return (
-    <div className="App">
-      <div>
-        <input placeholder="내 목표" ref={inputRef}></input>
-        <button onClick={confirm}>확인</button>
-      </div>
-      <ul>
-        {tasks.map((v, index) => (
-          <li key={v.id}>{v.text}</li>
-        ))}
-      </ul>
+    <div>
+      <p>{secound}초</p>
     </div>
   );
 }
