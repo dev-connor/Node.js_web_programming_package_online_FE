f191b29b405f72c53bd015031a41da6f49879cbf
Connor <kor.connor@gmail.com>
2025-05-21 23:47:37 +0900

    feat: CH03_12. 컴포넌트 스타일링1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CustomList from "./components/CustomList";
+import Button from "./components/Button";
 
 function App() {
   const array1 = ["apple", "banana", "orange"];
   return (
     <div className="App">
+      <Button></Button>
       <ul>
-        {array1.map((v) => {
-          return <CustomList text={v}></CustomList>;
+        {array1.map((value, index) => {
+          return <CustomList text={`${index} ${value}`}></CustomList>;
         })}
       </ul>
     </div>
---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./../style/Button.scss"
+
+export default function Button() {
+  return <button><span>Green</span></button>
+}
